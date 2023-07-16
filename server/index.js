e20f50c521f21e97011528732626370d43bce80f
Nitesh <niteshtiwari5222@gmail.com>
2023-07-16 12:12:26 +0530

    Intial Setup for Development

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    succes: true,
+    message: "Welcome to College-Dukan",
+  });
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log("Server is Running ");
+});
