fef15fd444884c97205a44733e9a2114b65a07f3
Piyush Singh <rr2rinku@gmail.com>
2023-07-17 14:02:38 +0530

    error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import { Login } from './account/Login';
 import { Register } from './account/Register';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './main/HomePage';
-import { useState } from 'react';
+
 
 
 function App() {
-  const [user, setLoginUser] = useState({});
-  const location = useLocation();
   return (
   <>
-  <Routes location={location} key={location.pathname}>
-      <Route exact path="/"  element={user && user._id ? <HomePage/>: <Login setLoginUser={setLoginUser}/> }/>
-      <Route exact path="/login" element={ <Login setLoginUser={setLoginUser}/>}/>
+     <Routes> 
+      <Route exact path="/"  element={ <HomePage />}/> 
+      <Route exact path="/login" element={ <Login />}/>
       <Route exact path="/register" element={ <Register />}/> 
     </Routes> 
   </>
