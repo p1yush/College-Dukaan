4f366cc991ec070a4b593438b8ea7e984fd00126
Piyush Singh <rr2rinku@gmail.com>
2023-07-17 13:56:13 +0530

    frontend folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { Login } from './account/Login';
+import { Register } from './account/Register';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { HomePage } from './main/HomePage';
+import { useState } from 'react';
+
 
 function App() {
+  const [user, setLoginUser] = useState({});
+  const location = useLocation();
   return (
-    <h1>College Dukaan</h1>
+  <>
+  <Routes location={location} key={location.pathname}>
+      <Route exact path="/"  element={user && user._id ? <HomePage/>: <Login setLoginUser={setLoginUser}/> }/>
+      <Route exact path="/login" element={ <Login setLoginUser={setLoginUser}/>}/>
+      <Route exact path="/register" element={ <Register />}/> 
+    </Routes> 
+  </>
   );
 }
 
---
+++ client/src/account/Register.js
@@ -0,0 +1,9 @@
+import { Footer } from "../components/Footer"
+
+export const Register = () => {
+    return (
+        <>
+        <Footer />
+        </>
+    )
+}
