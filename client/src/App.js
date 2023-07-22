868c19369968c658f4bf73f12b56ca82395f80af
Narender Singh Bisht <narendersinghbisht382@gmail.com>
2023-07-22 11:50:03 +0530

    Footer and navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Login } from './account/Login';
 import { Register } from './account/Register';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './main/HomePage';
+import { GlobalStyle } from './GlobalStyle/GlobalStyle';
 
 
 
 function App() {
   return (
   <>
+     <GlobalStyle/>
      <Routes> 
       <Route exact path="/"  element={ <HomePage />}/> 
       <Route exact path="/login" element={ <Login />}/>
--- client/src/main/HomePage.js
+++ client/src/main/HomePage.js
@@ -1,10 +1,14 @@
+import AboutUs from "../components/AboutUs"
 import { Footer } from "../components/Footer"
+import HeroSection from "../components/HeroSection"
 import { Navbar } from "../components/Navbar"
 
 export const HomePage = () => {
     return (
         <>
         <Navbar />
+        <HeroSection/>
+        <AboutUs/>
         <Footer />
         </>
     )
